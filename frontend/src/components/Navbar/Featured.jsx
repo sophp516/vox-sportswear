3088d38d514ca07f855bba6af2e728365744b391
prishaAgg <prishaaggarwal05@gmail.com>
2024-05-26 12:18:30 -0400

    curr

---
+++ frontend/src/components/Navbar/Featured.jsx
@@ -0,0 +1,19 @@
+import '../../components/Featured.css'
+const Featured = () => {
+
+    return(
+
+        <div>
+        
+            <p className='header'> Vox Sportswear</p>
+            <img className = 'baker' src="../../public/assets/baker.png" />
+            
+
+
+        </div>
+
+
+    )
+}
+
+export default Featured
